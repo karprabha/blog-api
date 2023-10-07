4567de213e3333669826a00bf939099b72a00b06
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-07 15:47:07 +0530

    add jwt util

---
+++ backend/utils/jwt.util.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+
+const secretKey = process.env.JWT_SECRET;
+const refreshTokenKey = process.env.REFRESH_TOKEN_SECRET;
+
+const generateAccessToken = (payload) =>
+    jwt.sign(payload, secretKey, { expiresIn: "15m" });
+
+const generateRefreshToken = (payload) =>
+    jwt.sign(payload, refreshTokenKey, { expiresIn: "7d" });
+
+const verifyAccessToken = (token) => {
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        return decoded;
+    } catch (error) {
+        return null;
+    }
+};
+
+const verifyRefreshToken = (token) => {
+    try {
+        const decoded = jwt.verify(token, refreshTokenKey);
+        return decoded;
+    } catch (error) {
+        return null;
+    }
+};
+
+export default {
+    generateAccessToken,
+    generateRefreshToken,
+    verifyAccessToken,
+    verifyRefreshToken,
+};
