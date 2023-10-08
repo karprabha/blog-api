d2f84768b54fcb6060a97ff22a4851292c2a19c7
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-08 21:48:41 +0530

    add role-based authorization

--- backend/src/api/v1/middlewares/auth.middleware.js
+++ backend/src/api/v1/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
+import User from "../models/user.js";
 import jwt from "../../../../utils/jwt.util.js";
 
-const authenticateToken = (req, res, next) => {
+const authenticateToken = async (req, res, next) => {
     const token = req.header("Authorization");
 
     if (!token) {
@@ -9,14 +10,34 @@ const authenticateToken = (req, res, next) => {
             .json({ message: "Access denied. No token provided." });
     }
 
-    const decoded = jwt.verifyAccessToken(token);
+    try {
+        const decoded = jwt.verifyAccessToken(token);
 
-    if (!decoded) {
+        if (!decoded) {
+            return res.status(401).json({ message: "Invalid token." });
+        }
+
+        const user = await User.findById(decoded.user_id);
+
+        if (!user) {
+            return res.status(401).json({ message: "User not found." });
+        }
+
+        req.user = user;
+        return next();
+    } catch (error) {
         return res.status(401).json({ message: "Invalid token." });
     }
+};
 
-    req.user = decoded;
+const authorizeRoles = (allowedRoles) => (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+        return res
+            .status(403)
+            .json({ message: "Access denied. Insufficient permissions." });
+    }
+
     return next();
 };
 
-export default authenticateToken;
+export default { authenticateToken, authorizeRoles };
--- backend/src/api/v1/routes/blog.route.js
+++ backend/src/api/v1/routes/blog.route.js
@@ -2,27 +2,39 @@ import { Router } from "express";
 
 import commentRouter from "./comment.route.js";
 import blogController from "../controllers/blog.controller.js";
-import authenticateToken from "../middlewares/auth.middleware.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.get("/blogs", blogController.getAllBlogPosts);
 
-router.post("/blogs", authenticateToken, blogController.createBlogPost);
+router.post(
+    "/blogs",
+    authMiddleware.authenticateToken,
+    authMiddleware.authorizeRoles(["admin"]),
+    blogController.createBlogPost
+);
 
 router.get("/blogs/:id", blogController.getBlogPostById);
 
-router.put("/blogs/:id", authenticateToken, blogController.updateBlogPostById);
+router.put(
+    "/blogs/:id",
+    authMiddleware.authenticateToken,
+    authMiddleware.authorizeRoles(["admin"]),
+    blogController.updateBlogPostById
+);
 
 router.patch(
     "/blogs/:id",
-    authenticateToken,
+    authMiddleware.authenticateToken,
+    authMiddleware.authorizeRoles(["admin"]),
     blogController.partiallyUpdateBlogPostById
 );
 
 router.delete(
     "/blogs/:id",
-    authenticateToken,
+    authMiddleware.authenticateToken,
+    authMiddleware.authorizeRoles(["admin"]),
     blogController.deleteBlogPostById
 );
 
