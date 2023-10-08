523dde52ed5ae31fa30a589787fcd11eb35c481c
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-08 23:37:23 +0530

    add auth and ownership middleware on comment route

--- backend/src/api/v1/routes/comment.route.js
+++ backend/src/api/v1/routes/comment.route.js
@@ -1,19 +1,44 @@
 import { Router } from "express";
 
 import commentController from "../controllers/comment.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
+import checkOwnership from "../middlewares/ownership.middleware.js";
 
 const router = Router({ mergeParams: true });
 
 router.get("/comments", commentController.getAllComments);
 
-router.post("/comments", commentController.createComment);
-
 router.get("/comments/:id", commentController.getCommentById);
 
-router.put("/comments/:id", commentController.updateCommentById);
+router.post(
+    "/comments",
+    authMiddleware.authenticateToken,
+    authMiddleware.authorizeRoles(["admin", "user"]),
+    commentController.createComment
+);
 
-router.patch("/comments/:id", commentController.partiallyUpdateCommentById);
+router.put(
+    "/comments/:id",
+    authMiddleware.authenticateToken,
+    authMiddleware.authorizeRoles(["admin", "user"]),
+    checkOwnership("comment"),
+    commentController.updateCommentById
+);
 
-router.delete("/comments/:id", commentController.deleteCommentById);
+router.patch(
+    "/comments/:id",
+    authMiddleware.authenticateToken,
+    authMiddleware.authorizeRoles(["admin", "user"]),
+    checkOwnership("comment"),
+    commentController.partiallyUpdateCommentById
+);
+
+router.delete(
+    "/comments/:id",
+    authMiddleware.authenticateToken,
+    authMiddleware.authorizeRoles(["admin", "user"]),
+    checkOwnership("comment"),
+    commentController.deleteCommentById
+);
 
 export default router;
