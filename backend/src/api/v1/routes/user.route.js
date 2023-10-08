8d144d529e9feebc5008d5a6b3d7c144903084e4
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-09 03:12:27 +0530

    add validator to user routes

--- backend/src/api/v1/routes/user.route.js
+++ backend/src/api/v1/routes/user.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 
+import userValidator from "../validators/user.validator.js";
 import userController from "../controllers/user.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import checkOwnership from "../middlewares/ownership.middleware.js";
+import queryValidationMiddleware from "../middlewares/validation.middleware.js";
 
 const router = Router();
 
@@ -10,13 +12,20 @@ router.get("/users", userController.getAllUsers);
 
 router.get("/users/:id", userController.getUserById);
 
-router.post("/users", userController.createUser);
+router.post(
+    "/users",
+    userValidator.createUserValidator,
+    queryValidationMiddleware,
+    userController.createUser
+);
 
 router.put(
     "/users/:id",
     authMiddleware.authenticateToken,
     authMiddleware.authorizeRoles(["admin", "user"]),
     checkOwnership("user"),
+    userValidator.updateUserValidator,
+    queryValidationMiddleware,
     userController.updateUserById
 );
 
@@ -25,6 +34,8 @@ router.patch(
     authMiddleware.authenticateToken,
     authMiddleware.authorizeRoles(["admin", "user"]),
     checkOwnership("user"),
+    userValidator.partiallyUpdateUserValidator,
+    queryValidationMiddleware,
     userController.partiallyUpdateUserById
 );
 
@@ -33,6 +44,8 @@ router.delete(
     authMiddleware.authenticateToken,
     authMiddleware.authorizeRoles(["admin", "user"]),
     checkOwnership("user"),
+    userValidator.deleteUserValidator,
+    queryValidationMiddleware,
     userController.deleteUserById
 );
 
---
+++ backend/src/api/v1/validators/user.validator.js
@@ -0,0 +1,129 @@
+import { body, param } from "express-validator";
+
+import User from "../models/user.js";
+
+const createUserValidator = [
+    body("first_name")
+        .notEmpty()
+        .withMessage("First name is required")
+        .isLength({ max: 100 })
+        .withMessage("First name must be at most 100 characters"),
+    body("family_name")
+        .notEmpty()
+        .withMessage("Family name is required")
+        .isLength({ max: 100 })
+        .withMessage("Family name must be at most 100 characters"),
+    body("username")
+        .notEmpty()
+        .withMessage("Username is required")
+        .isLength({ max: 50 })
+        .withMessage("Username must be at most 50 characters")
+        .custom(async (value) => {
+            const existingUser = await User.findOne({ username: value });
+            if (existingUser) {
+                throw new Error("Username is already in use");
+            }
+        }),
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({ min: 8, max: 128 })
+        .withMessage("Password must be between 8 and 128 characters")
+        .matches(/\d/)
+        .withMessage("Password must contain at least one digit")
+        .matches(/[A-Z]/)
+        .withMessage("Password must contain at least one uppercase letter")
+        .matches(/[a-z]/)
+        .withMessage("Password must contain at least one lowercase letter")
+        .matches(/[!@#$%^&*(),.?":{}|<>]/)
+        .withMessage(
+            'Password must contain at least one special character (!@#$%^&*(),.?":{}|<>))'
+        ),
+];
+
+const updateUserValidator = [
+    param("id").notEmpty().withMessage("User ID is required"),
+    body("first_name")
+        .notEmpty()
+        .withMessage("First name is required")
+        .isLength({ max: 100 })
+        .withMessage("First name must be at most 100 characters"),
+    body("family_name")
+        .notEmpty()
+        .withMessage("Family name is required")
+        .isLength({ max: 100 })
+        .withMessage("Family name must be at most 100 characters"),
+    body("username")
+        .notEmpty()
+        .withMessage("Username is required")
+        .isLength({ max: 50 })
+        .withMessage("Username must be at most 50 characters")
+        .custom(async (value) => {
+            const existingUser = await User.findOne({ username: value });
+            if (existingUser) {
+                throw new Error("Username is already in use");
+            }
+        }),
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({ min: 8, max: 128 })
+        .withMessage("Password must be between 8 and 128 characters")
+        .matches(/\d/)
+        .withMessage("Password must contain at least one digit")
+        .matches(/[A-Z]/)
+        .withMessage("Password must contain at least one uppercase letter")
+        .matches(/[a-z]/)
+        .withMessage("Password must contain at least one lowercase letter")
+        .matches(/[!@#$%^&*(),.?":{}|<>]/)
+        .withMessage(
+            'Password must contain at least one special character (!@#$%^&*(),.?":{}|<>))'
+        ),
+];
+
+const partiallyUpdateUserValidator = [
+    param("id").notEmpty().withMessage("User ID is required"),
+    body("first_name")
+        .optional()
+        .isLength({ max: 100 })
+        .withMessage("First name must be at most 100 characters"),
+    body("family_name")
+        .optional()
+        .isLength({ max: 100 })
+        .withMessage("Family name must be at most 100 characters"),
+    body("username")
+        .optional()
+        .isLength({ max: 50 })
+        .withMessage("Username must be at most 50 characters")
+        .custom(async (value) => {
+            const existingUser = await User.findOne({ username: value });
+            if (existingUser) {
+                throw new Error("Username is already in use");
+            }
+        }),
+    body("password")
+        .optional()
+        .isLength({ min: 8, max: 128 })
+        .withMessage("Password must be between 8 and 128 characters")
+        .matches(/\d/)
+        .withMessage("Password must contain at least one digit")
+        .matches(/[A-Z]/)
+        .withMessage("Password must contain at least one uppercase letter")
+        .matches(/[a-z]/)
+        .withMessage("Password must contain at least one lowercase letter")
+        .matches(/[!@#$%^&*(),.?":{}|<>]/)
+        .withMessage(
+            'Password must contain at least one special character (!@#$%^&*(),.?":{}|<>))'
+        ),
+];
+
+const deleteUserValidator = [
+    param("id").notEmpty().withMessage("User ID is required"),
+];
+
+export default {
+    createUserValidator,
+    updateUserValidator,
+    partiallyUpdateUserValidator,
+    deleteUserValidator,
+};
