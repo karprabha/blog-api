7efe0d15b4fa06e8de4ff7f63340c21c96491068
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-09 00:08:55 +0530

    add auth and ownership middleware on user route

--- backend/src/api/v1/routes/user.route.js
+++ backend/src/api/v1/routes/user.route.js
@@ -1,19 +1,39 @@
 import { Router } from "express";
 
 import userController from "../controllers/user.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
+import checkOwnership from "../middlewares/ownership.middleware.js";
 
 const router = Router();
 
 router.get("/users", userController.getAllUsers);
 
-router.post("/users", userController.createUser);
-
 router.get("/users/:id", userController.getUserById);
 
-router.put("/users/:id", userController.updateUserById);
+router.post("/users", userController.createUser);
 
-router.patch("/users/:id", userController.partiallyUpdateUserById);
+router.put(
+    "/users/:id",
+    authMiddleware.authenticateToken,
+    authMiddleware.authorizeRoles(["admin", "user"]),
+    checkOwnership("user"),
+    userController.updateUserById
+);
 
-router.delete("/users/:id", userController.deleteUserById);
+router.patch(
+    "/users/:id",
+    authMiddleware.authenticateToken,
+    authMiddleware.authorizeRoles(["admin", "user"]),
+    checkOwnership("user"),
+    userController.partiallyUpdateUserById
+);
+
+router.delete(
+    "/users/:id",
+    authMiddleware.authenticateToken,
+    authMiddleware.authorizeRoles(["admin", "user"]),
+    checkOwnership("user"),
+    userController.deleteUserById
+);
 
 export default router;
