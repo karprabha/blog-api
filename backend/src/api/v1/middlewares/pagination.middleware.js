3e0357df9fb43b7f0a84e7bd0242f10ddc1b8528
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-10 16:58:44 +0530

    add pagination middleware

---
+++ backend/src/api/v1/middlewares/pagination.middleware.js
@@ -0,0 +1,52 @@
+const paginate = (model) => async (req, res, next) => {
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 10;
+    const fields = req.query.fields
+        ? req.query.fields.split(",").join(" ")
+        : "";
+    const sort = req.query.sort || "-createdAt";
+
+    const skip = (page - 1) * limit;
+
+    try {
+        const total = await model.countDocuments();
+        const totalPages = Math.ceil(total / limit);
+
+        const currentUrl = `${req.protocol}://${req.get("host")}${
+            req.originalUrl.split("?")[0]
+        }`;
+
+        const results = await model
+            .find()
+            .select(fields)
+            .sort(sort)
+            .skip(skip)
+            .limit(limit)
+            .exec();
+
+        req.paginatedResults = {
+            results,
+            page,
+            limit,
+            totalPages,
+            total,
+            next:
+                page < totalPages
+                    ? `${currentUrl}?page=${page + 1}&limit=${limit}${
+                          fields !== "" ? `&fields=${fields}` : ""
+                      }`
+                    : null,
+            prev:
+                page > 1
+                    ? `${currentUrl}?page=${page - 1}&limit=${limit}${
+                          fields !== "" ? `&fields=${fields}` : ""
+                      }`
+                    : null,
+        };
+        next();
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export default paginate;
