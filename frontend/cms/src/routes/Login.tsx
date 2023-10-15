d58b7a31a5e83db15c2256d9459b8057fc4c8722
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-16 03:14:00 +0530

    add error control and signup redirect

--- frontend/cms/src/routes/Login.tsx
+++ frontend/cms/src/routes/Login.tsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+const ROOT_URL = "/";
 
 const Login = () => {
     const navigate = useNavigate();
+    const userRef = useRef<HTMLInputElement>(null);
+    const errRef = useRef<HTMLInputElement>(null);
+
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [errorMessages, setErrorMessages] = useState<string[]>([]);
 
-    const rootUrl = "/";
+    useEffect(() => {
+        if (userRef.current) {
+            userRef.current.focus();
+        }
+    }, []);
 
-    const handleLogin = async () => {
+    useEffect(() => {
+        setErrorMessages([]);
+    }, [username, password]);
+
+    const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
         try {
             const response = await fetch("/api/v1/auth/login", {
                 method: "POST",
@@ -22,20 +38,41 @@ const Login = () => {
                 localStorage.setItem("accessToken", accessToken);
                 localStorage.setItem("refreshToken", refreshToken);
 
-                navigate(rootUrl);
+                navigate(ROOT_URL);
             } else {
-                alert("Login failed. Please check your credentials.");
+                const { errors, message } = await response.json();
+
+                if (errors) {
+                    const newErrorMessages = errors.map(
+                        (error: { msg: string }) => error.msg
+                    );
+                    setErrorMessages(newErrorMessages);
+                } else if (message) setErrorMessages([message]);
+                errRef.current?.focus();
             }
         } catch (error) {
             console.error("Login error:", error);
+            setErrorMessages([`"Login error:", ${error}`]);
+            errRef.current?.focus();
         }
     };
 
     return (
         <div className="flex my-20 items-center justify-center h-full bg-gray-100">
             <div className="bg-white w-full max-w-sm box-border p-8 rounded-lg shadow-lg">
+                <div ref={errRef}>
+                    {errorMessages.map((errorMessage, index) => (
+                        <p
+                            key={index}
+                            className="text-red-500 bg-red-100 p-2 rounded mb-4 text-center"
+                            aria-live="assertive"
+                        >
+                            {errorMessage}
+                        </p>
+                    ))}
+                </div>
                 <h2 className="text-2xl text-center mb-4">Login</h2>
-                <form>
+                <form onSubmit={handleLogin}>
                     <div className="mb-4">
                         <label
                             htmlFor="username"
@@ -44,8 +81,11 @@ const Login = () => {
                             Username
                         </label>
                         <input
-                            type="username"
+                            type="text"
                             id="username"
+                            ref={userRef}
+                            required
+                            autoComplete="off"
                             className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none"
                             placeholder="Username"
                             value={username}
@@ -62,6 +102,7 @@ const Login = () => {
                         <input
                             type="password"
                             id="password"
+                            required
                             className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none"
                             placeholder="Password"
                             value={password}
@@ -69,13 +110,20 @@ const Login = () => {
                         />
                     </div>
                     <button
-                        type="button"
+                        type="submit"
                         className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
-                        onClick={handleLogin}
                     >
                         Login
                     </button>
                 </form>
+                <p className="mt-4 text-center text-gray-600 text-sm">
+                    Need an Account?{" "}
+                    <span className="line">
+                        <Link to={"/signup"} className="text-blue-500">
+                            Sign Up
+                        </Link>
+                    </span>
+                </p>
             </div>
         </div>
     );
