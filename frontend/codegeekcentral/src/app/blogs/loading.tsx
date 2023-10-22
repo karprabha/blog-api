b33ba877114c09e4c048ca35ec6a3737f0dc732a
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-23 02:59:24 +0530

    remove unwanted loading page

--- frontend/codegeekcentral/src/app/blogs/loading.tsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-    return (
-        <div className="flex justify-center items-center h-screen bg-gray-100">
-            <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
-        </div>
-    );
-};
-
-export default Loading;
