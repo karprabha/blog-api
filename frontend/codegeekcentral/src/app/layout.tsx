36f7d7099d167f455466fd3e6cf4bbcd0bb5dc75
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-24 21:33:04 +0530

    fix GA_MEASUREMENT_ID issue

--- frontend/codegeekcentral/src/app/layout.tsx
+++ frontend/codegeekcentral/src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', ${GA_MEASUREMENT_ID});
+          gtag('config', '${GA_MEASUREMENT_ID}');
         `}
             </Script>
 
