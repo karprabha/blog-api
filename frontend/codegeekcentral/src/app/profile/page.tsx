ef9776c02432fde4aecde299ae2cab5d59518389
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-24 19:01:04 +0530

    update profile route in next

--- frontend/codegeekcentral/src/app/profile/page.tsx
+++ frontend/codegeekcentral/src/app/profile/page.tsx
@@ -18,11 +18,13 @@ interface User {
     family_name: string;
     username: string;
     role: string;
+    avatar_url: string;
 }
 
 interface Blog {
     _id: string;
     title: string;
+    published: boolean;
 }
 
 interface Comment {
@@ -52,15 +54,22 @@ const Profile = () => {
     const failedAuth = useFailedAuth();
 
     const effectRun = useRef(false);
+    const errRef = useRef<HTMLInputElement>(null);
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
     const [user, setUser] = useState<UserProfileData | null>(null);
+
+    const [avatarInput, setAvatarInput] = useState("");
+    const [avatarFile, setAvatarFile] = useState<File>();
+    const [imageUploading, setImageUploading] = useState(false);
+    const [showAvatarUpdateModal, setShowAvatarUpdateModal] = useState(false);
+    const [errorMessages, setErrorMessages] = useState<string[]>([]);
+
+    const decoded: JwtPayload = jwt_decode(auth.accessToken);
+    const { user_id } = decoded;
 
     useEffect(() => {
         let isMounted = true;
         const controller = new AbortController();
-
-        const decoded: JwtPayload = jwt_decode(auth.accessToken);
-
-        const { user_id } = decoded;
 
         const getUser = async () => {
             try {
@@ -81,7 +90,6 @@ const Profile = () => {
                 }
             } catch (err) {
                 console.error(err);
-
                 failedAuth();
             }
         };
@@ -97,43 +105,251 @@ const Profile = () => {
         };
     }, []);
 
+    useEffect(() => {
+        setErrorMessages([]);
+    }, [avatarInput, avatarFile]);
+
+    const updateAvatarWithUrl = async (avatar_url: string) => {
+        try {
+            const response = await fetch(`${API_URI}/api/v1/users/${user_id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ avatar_url }),
+            });
+
+            if (response.ok) {
+                setUser((prevState) => {
+                    if (prevState) {
+                        return {
+                            ...prevState,
+                            user: {
+                                ...prevState.user,
+                                avatar_url: avatar_url,
+                            },
+                        };
+                    }
+                    return prevState;
+                });
+
+                setShowAvatarUpdateModal(false);
+                return true;
+            } else if (response.status === 401) {
+                failedAuth();
+            } else {
+                const { errors, msg } = await response.json();
+
+                if (errors) {
+                    const newErrorMessages = errors.map(
+                        (error: { msg: string }) => error.msg
+                    );
+                    setErrorMessages(newErrorMessages);
+                } else if (msg) setErrorMessages([msg]);
+                errRef.current?.focus();
+            }
+        } catch (err) {
+            console.error(err);
+            failedAuth();
+        }
+    };
+
+    const updateAvatarWithFile = async (file: File) => {
+        try {
+            const formData = new FormData();
+            formData.append("avatar", file);
+
+            const response = await fetch(`${API_URI}/api/v1/upload`, {
+                method: "POST",
+                body: formData,
+            });
+
+            setImageUploading(false);
+            if (response.ok) {
+                const { url } = await response.json();
+                updateAvatarWithUrl(url);
+                return true;
+            } else if (response.status === 401) {
+                failedAuth();
+            } else {
+                const { errors, msg } = await response.json();
+
+                if (errors) {
+                    const newErrorMessages = errors.map(
+                        (error: { msg: string }) => error.msg
+                    );
+                    setErrorMessages(newErrorMessages);
+                } else if (msg) setErrorMessages([msg]);
+                errRef.current?.focus();
+            }
+        } catch (err) {
+            setImageUploading(false);
+            console.error(err);
+            failedAuth();
+        }
+    };
+
+    const handleAvatarUpdate = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (avatarInput) {
+            updateAvatarWithUrl(avatarInput);
+        } else if (avatarFile) {
+            setImageUploading(true);
+            updateAvatarWithFile(avatarFile);
+        } else {
+            setErrorMessages(["No option is selected"]);
+            errRef.current?.focus();
+        }
+    };
+
+    const handleAvatarInputChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setAvatarInput(e.target.value);
+        setAvatarFile(undefined);
+
+        if (fileInputRef.current) {
+            fileInputRef.current.value = "";
+        }
+    };
+
+    const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const target = e.target as HTMLInputElement;
+        const file: File = (target.files as FileList)[0];
+
+        if (file) {
+            setAvatarFile(file);
+            setAvatarInput("");
+        }
+    };
+
     return (
         <>
+            {showAvatarUpdateModal && (
+                <div
+                    className={`fixed top-0 left-0 w-full h-screen bg-black bg-opacity-40 z-50 flex items-center justify-center`}
+                >
+                    <div className="modal-box p-4 bg-white rounded shadow-lg">
+                        <div ref={errRef}>
+                            {errorMessages.map((errorMessage, index) => (
+                                <p
+                                    key={index}
+                                    className="text-red-500 bg-red-100 p-2 rounded mb-4 text-center"
+                                    aria-live="assertive"
+                                >
+                                    {errorMessage}
+                                </p>
+                            ))}
+                        </div>
+                        {imageUploading && (
+                            <p
+                                className="text-blue-500 bg-blue-100 p-2 rounded mb-4 text-center"
+                                aria-live="assertive"
+                            >
+                                Uploading image... Please wait.
+                            </p>
+                        )}
+                        <h2 className="text-2xl font-semibold mb-4">
+                            Update Your Avatar
+                        </h2>
+                        <p className="text-gray-700 mb-4">
+                            Enter an image URL or upload a new image:
+                        </p>
+
+                        <form onSubmit={handleAvatarUpdate}>
+                            <input
+                                type="text"
+                                placeholder="Image URL"
+                                className="border p-2 mb-4 w-full"
+                                value={avatarInput}
+                                onChange={handleAvatarInputChange}
+                            />
+
+                            <p className="text-center text-gray-700 mb-4">OR</p>
+
+                            <input
+                                type="file"
+                                accept="image/*"
+                                className="mb-4"
+                                ref={fileInputRef}
+                                onChange={handleFileInputChange}
+                            />
+
+                            <div className="flex justify-end">
+                                <button
+                                    type="button"
+                                    disabled={imageUploading}
+                                    className={`${
+                                        imageUploading
+                                            ? "bg-gray-300 text-gray-500 px-4 py-2 rounded-md mr-2 cursor-not-allowed"
+                                            : "bg-gray-300 text-gray-700 px-4 py-2 rounded-md mr-2 hover:bg-gray-400"
+                                    }`}
+                                    onClick={() =>
+                                        setShowAvatarUpdateModal(false)
+                                    }
+                                >
+                                    Cancel
+                                </button>
+
+                                <button
+                                    type="submit"
+                                    disabled={imageUploading}
+                                    className={`${
+                                        imageUploading
+                                            ? "bg-blue-300 text-white px-4 py-2 rounded-md cursor-not-allowed"
+                                            : "bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                                    }`}
+                                >
+                                    Update
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
+
             <div className="bg-white mt-10 shadow-md rounded-lg p-6">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <div className="p-4">
+                    <div className="flex justify-center items-center">
                         {user && (
-                            <div className="text-center">
-                                <h2 className="text-2xl font-semibold mb-2">
-                                    User Profile
-                                </h2>
-                                <p className="text-gray-500 text-sm mb-2">
-                                    <strong>Username:</strong>{" "}
-                                    {user.user.username}
-                                </p>
-                                <p className="text-gray-500 text-sm">
-                                    <strong>Role:</strong> {user.user.role}
-                                </p>
+                            <div className="text-center relative group">
+                                <img
+                                    src={user.user.avatar_url}
+                                    alt={`Avatar for ${user.user.first_name} ${user.user.family_name}`}
+                                    className="m-auto w-48 h-48 rounded-full avatar-img"
+                                />
+                                <div
+                                    className="hidden w-48 h-48 rounded-full absolute inset-0  items-center justify-center group-hover:flex bg-black bg-opacity-60 cursor-pointer text-white text-xl"
+                                    onClick={() =>
+                                        setShowAvatarUpdateModal(true)
+                                    }
+                                >
+                                    Change Avatar
+                                </div>
                             </div>
                         )}
                     </div>
-                    <div className="p-4">
+                    <div className="flex justify-center items-center text-center md:justify-start md:text-left">
                         {user && (
                             <div>
-                                <h3 className="text-lg font-semibold mb-2">
+                                <h3 className="text-2xl font-semibold mb-2">
                                     Profile Information
                                 </h3>
-                                <p className="text-gray-500 text-sm mb-2">
+                                <p className="text-gray-500 text-base mb-1">
                                     <strong>First Name:</strong>{" "}
                                     {user.user.first_name}
                                 </p>
-                                <p className="text-gray-500 text-sm mb-2">
+                                <p className="text-gray-500 text-base mb-1">
                                     <strong>Family Name:</strong>{" "}
                                     {user.user.family_name}
                                 </p>
-                                <p className="text-gray-500 text-sm">
+                                <p className="text-gray-500 text-base mb-1">
                                     <strong>Username:</strong>{" "}
                                     {user.user.username}
+                                </p>
+                                <p className="text-gray-500 text-base">
+                                    <strong>Role:</strong> {user.user.role}
                                 </p>
                             </div>
                         )}
@@ -141,37 +357,58 @@ const Profile = () => {
                 </div>
             </div>
 
-            <div className="bg-white shadow-md rounded-lg p-6 mt-4">
-                <h2 className="text-2xl font-semibold mb-2">Blogs</h2>
-                {user && (
-                    <ul>
-                        {user.blogs.map((blog) => (
-                            <li key={blog._id}>
-                                <Link href={`/blogs/${blog._id}`}>
-                                    {blog.title}
-                                </Link>
-                            </li>
+            <div className="bg-gray-100 py-6 mt-4">
+                <h2 className="text-2xl font-semibold mb-4">Blogs</h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    {user &&
+                        user.blogs.map((blog) => (
+                            <div
+                                key={blog._id}
+                                className="flex flex-col justify-between bg-white rounded-lg shadow-lg hover:shadow-xl"
+                            >
+                                <div className="p-4">
+                                    <h2 className="text-2xl font-semibold text-gray-800">
+                                        {blog.title}
+                                    </h2>
+                                    <p className="text-gray-600 mt-2">
+                                        {blog.published ? "Published" : "Draft"}
+                                    </p>
+                                </div>
+                                <div className="bg-gray-100 p-4 border-t border-gray-200">
+                                    <Link
+                                        href={`/blogs/${blog._id}`}
+                                        className="mt-auto text-blue-500 font-semibold hover:underline"
+                                    >
+                                        Read More
+                                    </Link>
+                                </div>
+                            </div>
                         ))}
-                    </ul>
-                )}
+                </div>
             </div>
 
-            <div className="bg-white mb-10 shadow-md rounded-lg p-6 mt-4">
-                <h2 className="text-2xl font-semibold mb-2">Recent Comments</h2>
+            <div className="bg-white mb-10 shadow-lg rounded-lg p-6 mt-4">
+                <h2 className="text-3xl font-semibold text-gray-900 mb-6">
+                    Recent Comments
+                </h2>
                 {user && (
                     <ul>
                         {user.recentComments.map((comment) => (
-                            <li key={comment._id}>
-                                <p>
-                                    <strong>Comment on:</strong>{" "}
+                            <li
+                                key={comment._id}
+                                className="border-b border-gray-200 py-6"
+                            >
+                                <div className="mb-2">
+                                    <p className="text-gray-600">Comment on:</p>
                                     <Link
                                         href={`/blogs/${comment.blogPost._id}`}
+                                        className="text-blue-500 hover:underline text-xl font-semibold"
                                     >
                                         {comment.blogPost.title}
                                     </Link>
-                                </p>
+                                </div>
                                 <ReactMarkdown
-                                    className="text-gray-700 mt-2"
+                                    className="text-gray-700"
                                     components={{ code: CodeBlock }}
                                     remarkPlugins={[remarkGfm]}
                                 >
