72e04f3ce5556863b56128d499c9f37b04a5aee2
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-24 16:49:23 +0530

    add modal overlay in blog component

--- frontend/cms/src/routes/BlogPost.tsx
+++ frontend/cms/src/routes/BlogPost.tsx
@@ -216,25 +216,34 @@ const BlogPost = () => {
             </div>
 
             {showDeleteModal && (
-                <div className="fixed inset-0 z-10 flex items-center justify-center">
-                    <div className="modal-bg" onClick={cancelDelete}></div>
+                <div
+                    className={`fixed top-0 left-0 w-full h-screen bg-black bg-opacity-40 z-50 flex items-center justify-center`}
+                >
                     <div className="modal-box p-4 bg-white rounded shadow-lg">
-                        <p className="text-xl mb-4">
-                            Are you sure you want to delete this post?
-                        </p>
-                        <div className="flex justify-end">
-                            <button
-                                onClick={confirmDelete}
-                                className="px-4 py-2 mr-4 bg-red-500 hover:bg-red-600 text-white rounded"
-                            >
-                                Confirm
-                            </button>
-                            <button
+                        <div className="fixed inset-0 z-10 flex items-center justify-center">
+                            <div
+                                className="modal-bg"
                                 onClick={cancelDelete}
-                                className="px-4 py-2 bg-gray-500 hover:bg-gray-600 text-white rounded"
-                            >
-                                Cancel
-                            </button>
+                            ></div>
+                            <div className="modal-box p-4 bg-white rounded shadow-lg">
+                                <p className="text-xl mb-4">
+                                    Are you sure you want to delete this post?
+                                </p>
+                                <div className="flex justify-end">
+                                    <button
+                                        onClick={confirmDelete}
+                                        className="px-4 py-2 mr-4 bg-red-500 hover:bg-red-600 text-white rounded"
+                                    >
+                                        Confirm
+                                    </button>
+                                    <button
+                                        onClick={cancelDelete}
+                                        className="px-4 py-2 bg-gray-500 hover:bg-gray-600 text-white rounded"
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
