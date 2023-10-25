41a557df97c7f3de382eeca584b8412b04766e6b
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-10-25 13:39:04 +0530

    update loading style in cms

--- frontend/cms/src/components/PersistLogin.tsx
+++ frontend/cms/src/components/PersistLogin.tsx
@@ -35,7 +35,9 @@ const PersistLogin = () => {
             {!persist ? (
                 <Outlet />
             ) : isLoading ? (
-                <p className="text-xl">Loading...</p>
+                <div className="flex justify-center items-center h-screen bg-gray-100">
+                    <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
+                </div>
             ) : (
                 <Outlet />
             )}
--- frontend/cms/src/routes/BlogPost.tsx
+++ frontend/cms/src/routes/BlogPost.tsx
@@ -134,7 +134,13 @@ const BlogPost = () => {
     };
 
     if (loading) {
-        return <div className="text-center">Loading...</div>;
+        return (
+            <>
+                <div className="flex justify-center items-center h-screen bg-gray-100">
+                    <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
+                </div>
+            </>
+        );
     }
 
     if (!blog) {
